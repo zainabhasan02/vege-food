d40ff904b8ba634469e272cb04b231e20a9065d1
zainabhasan02 <zainab.hasan.infi02@gmail.com>
2023-08-08 00:40:19 +0530

    contact temp done

--- VegieStore/urls.py
+++ VegieStore/urls.py
@@ -19,10 +19,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from VegieStore.views import IndexView, AboutUsView
+from VegieStore.views import IndexView, AboutUsView, ContactUsView
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   url(r'^$', IndexView.as_view(), name='index'),
-                  url(r'^about/$', AboutUsView.as_view(), name='about')
+                  url(r'^about/$', AboutUsView.as_view(), name='about'),
+                  url(r'^contact/$', ContactUsView.as_view(), name='contact'),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- VegieStore/views.py
+++ VegieStore/views.py
@@ -7,6 +7,12 @@ class IndexView(View):
     def get(self, request):
         return render(request, 'index.html')
 
+
 class AboutUsView(View):
     def get(self, request):
         return render(request, 'about.html')
+
+
+class ContactUsView(View):
+    def get(self, request):
+        return render(request, 'contact.html')
