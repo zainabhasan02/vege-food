6b9fabaf1ea9123e1d0f34851b53a9c4afa93d4a
zainabhasan02 <zainab.hasan.infi02@gmail.com>
2023-08-12 07:09:20 +0530

    worked on Admin & Models

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from .models import ProductCategory, Product
+
+
 # Register your models here.
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = ("category_name", "active", "order", "addtime", "add_to_homepage")
+    search_fields = ("category_name",)
+
+
+# for improving Admin UI
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name","out_of_stock", "price","discount_percentage","product_category","add_to_home","addtime","active")
+    search_fields = ("name",)
+    list_filter = ("product_category__category_name",)
+
+
+admin.site.register(ProductCategory, ProductCategoryAdmin)
+admin.site.register(Product, ProductAdmin)
--- product/models.py
+++ product/models.py
@@ -1,3 +1,39 @@
+import null as null
 from django.db import models
 
+
 # Create your models here.
+class ProductCategory(models.Model):
+    category_name = models.CharField(max_length=500, null=True, blank=True, verbose_name="category_name")
+    active = models.BooleanField(default=True, null=True, blank=True, verbose_name="active")
+    order = models.IntegerField(null=True, blank=True, verbose_name="order")
+    addtime = models.DateTimeField(auto_now_add=True, null=True, blank=True, verbose_name="addtime")
+    category_image = models.ImageField(upload_to='category_images', null=True, blank=True,
+                                       verbose_name="category_image")
+    add_to_homepage = models.BooleanField(default=True, null=True, blank=True, verbose_name="add_to_homepage")
+
+    def __str__(self):
+        return self.category_name  # use for showing names
+
+    class Meta:
+        ordering = ["category_name"]
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=500, null=True, blank=True, verbose_name="name")
+    description = models.TextField(null=True, blank=True, verbose_name="description")
+    product_image = models.ImageField(upload_to='product_images', null=True, blank=True, verbose_name="product_image")
+    out_of_stock = models.BooleanField(default=False, null=True, blank=True, verbose_name="out_of_stock")
+    price = models.IntegerField(null=True, blank=True, verbose_name="price")
+    discount_percentage = models.IntegerField(null=True, blank=True, verbose_name="discount_percentage")
+    product_category = models.ForeignKey(ProductCategory, on_delete=models.SET_NULL, null=True, blank=True,
+                                         verbose_name="product_category")
+    add_to_home = models.BooleanField(default=False, null=True, blank=True, verbose_name="add_to_home")
+    addtime = models.DateTimeField(auto_now_add=True, null=True, blank=True, verbose_name="addtime")
+    active = models.BooleanField(default=True, null=True, blank=True, verbose_name="active")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
