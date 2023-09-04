06a8fe5bd8da36f37b3397543b01ed482649997d
zainabhasan02 <zainab.hasan.infi02@gmail.com>
2023-09-04 11:27:46 +0530

    contact us

--- users/urls.py
+++ users/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     url(r'^$', IndexView.as_view(), name='subscriber_email_form'),
     url(r'^about/$', AboutUsView.as_view(), name='about'),
     url(r'^contact/$', ContactUsView.as_view(), name='contact'),
+    url(r'^contact_saved/$', ContactUsView.as_view(), name='contact_saved'),
 ]
